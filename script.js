7a619580a28509da4bb93c81c2ab8448264052d6
SSulaberidze <ssulberidze@gmail.com>
2022-09-26 18:33:13 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,60 @@
+// სავარჯიშო 1
+let numfunct = (...numbers) => {
+    let numfunctsum = 0;
+    for (let a of numbers){
+        if (a > 0){
+            numfunctsum += a
+        }
+    }
+    console.log(numfunctsum);
+}
+numfunct(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+
+
+
+// სავარჯიშო 2
+function numberfunction(...numbers){
+    let numberfunctionsum = 0;
+    for (let a of numbers) {
+        numberfunctionsum += a
+    }
+    console.log(numberfunctionsum);
+}
+
+numberfunction(10, 50, 6, 7, 8, 11, 6, 3, 9);
+
+
+
+// სავარჯიშო 3
+let user = {
+    firstname: 'giorgi',
+    lastname: 'saakadze',
+    age: 32,
+    isloggedin: true
+  }
+
+for (let [key,value] of object.entries(user)) {
+    console.log(key,value);
+}
+
+
+
+// სავარჯიშო 5
+let array = [1,2,4,10,34,5,7,87];
+
+for (let a of array){
+    if (a>0 && a<10){
+        console.log(a);
+    }
+}
+
+
+
+// სავარჯიშო 6
+let numbers =[1,2,3,4,5,6,7,8,9,10];
+for (let a of numbers){
+    if (a==5){
+      break;
+    }
+    console.log(a);
+}
